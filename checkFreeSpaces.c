46f03b1d92743b587587b59cfccafd03c89e29ef
Shreyp1 <shreypatel16@hotmail.com>
2023-02-18 14:42:39 +0530

    Created checkFreeSpaces.c

---
+++ checkFreeSpaces.c
@@ -0,0 +1,11 @@
+#include "fn.h"
+int checkFreeSpaces() {
+    int freeSpace = 9;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') {
+                freeSpace--;
+            }
+        }
+    }
+}
--- fn.h
+++ fn.h
@@ -5,6 +5,7 @@ void playerMove1();
 void playerMove2();
 void printWinner(char);
 void printBoard();
+int checkFreeSpaces();
 
 int count = 1;
 char board[3][3];
