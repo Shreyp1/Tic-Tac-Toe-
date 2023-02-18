7ce9ed2d5062e8cb6aff6f8fac42a56e8b68cfc5
Shreyp1 <shreypatel16@hotmail.com>
2023-02-18 15:25:23 +0530

    Fixed print winner bug

--- printWinner.c
+++ printWinner.c
@@ -1,9 +1,9 @@
 #include "fn.h"
 void printWinner(char p) {
-    if (winner == p) {
+    if ('X' == p) {
         printf("Player 1 (X) wins!!!\n");
     }
-    else if (winner == p) {
+    else if ('O' == p) {
         printf("Player 2 (O) wins!!!\n");
     }
     else printf("Tie!!\n");
