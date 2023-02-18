857f8cdc7247bce955f83da7ac7b9b016e0ab9f5
Shreyp1 <shreypatel16@hotmail.com>
2023-02-18 14:45:38 +0530

    Small big fix

--- checkFreeSpaces.c
+++ checkFreeSpaces.c
@@ -3,9 +3,10 @@ int checkFreeSpaces() {
     int freeSpace = 9;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (board[i][j] == ' ') {
+            if (board[i][j] != ' ') {
                 freeSpace--;
             }
         }
     }
+    return freeSpace;
 }
